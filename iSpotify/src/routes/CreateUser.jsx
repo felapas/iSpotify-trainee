d01b211f79af9a770b596f1fa4a08c125804b53c
Felipe Damasceno <122218940+felapas@users.noreply.github.com>
2024-06-14 11:43:08 -0300

    Update CreateUser.jsx

--- iSpotify/src/routes/CreateUser.jsx
+++ iSpotify/src/routes/CreateUser.jsx
@@ -10,27 +10,6 @@ const CreateUser = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      const response = await api.post("/users", {
-        name,
-        email,
-        password,
-        role,
-      });
-      
-      if (response.status === 201) {
-        setMessage("Usuário criado com sucesso!");
-      }
-    } catch (error) {
-      if (error.response) {
-        setMessage(`Erro na resposta da API: ${error.response.data.msg}`);
-      } else if (error.request) {
-        setMessage("Erro na requisição: Sem resposta do servidor");
-      } else {
-        setMessage(`Erro: ${error.message}`);
-      }
-    }
   };
 
   return (
